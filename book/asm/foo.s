e8c88cf18a63cb4132503658a141f5640c6d6b9e
gondow <gondow@cs.titech.ac.jp>
2023-08-05 20:30:42 +0900

    hoge

--- book/asm/foo.s
+++
@@ -1,5 +0,0 @@
-    .text
-    .globl main
-main:
-    movq $999, %rax
-    ret
--- book/asm/movq-4.s
+++
@@ -1,8 +0,0 @@
-# asm/movq-4.s
-    .text
-    .globl main
-    .type main, @function
-main:
-    movq $999, %rax
-    ret
-    .size main, .-main
